8578d1c070e0658fcf5ab38b544e65c34781d8d5
Jonathan Chan <jchan25@gmail.com>
2014-11-09 12:52:34 -0600

    hw_completed

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,41 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    p = Photo.find(params[:id])
+    @image_url = p.source
+    @caption = p.caption
+  end
+
+   def new_form
+
+   end
+
+   def create_row
+     p = Photo.new
+     p.source = params[:the_source]
+     p.caption = params[:the_caption]
+     p.save
+
+   end
+
+    def destroy
+      p = Photo.find(params[:id])
+      p.destroy
+
+    end
+
+   def edit_form
+      p = Photo.find(params[:id])
+      @image_url = p.source
+      @caption = p.caption
+   end
+
+    def update_row
+      p = Photo.find(params[:id])
+      p.source = params[:the_source]
+      p.caption = params[:the_caption]
+      p.save
+    end
 
 end
